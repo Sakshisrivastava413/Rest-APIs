f48c3e8cdc37c2a5375b0b82e28608359777e0cd
Sakshi <sakshisrivastava413@gmail.com>
2018-07-25 00:53:18 +0530

    added geoJSON to handle ninja's location

--- models/ninja.js
+++ models/ninja.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+// create geolocation schema
+const GeolocationSchema = new Schema({
+  type: {
+    type: String,
+    default: "Point"
+  },
+  coordinates: {
+    type: [Number],
+    index: "2dsphere"
+  }
+});
+
 // create ninja schema and models
 const NinjaSchema = new Schema({
   name: {
@@ -13,8 +26,8 @@ const NinjaSchema = new Schema({
   available: {
     type: Boolean,
     default: false
-  }
-  // add geolocation
+  },
+  geometry: GeolocationSchema
 });
 
 const Ninja = mongoose.model('ninja', NinjaSchema);
