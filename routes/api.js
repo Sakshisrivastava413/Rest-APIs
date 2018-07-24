69d56ca9623b9461542acf093d6de2d1f51430e2
Sakshi <sakshisrivastava413@gmail.com>
2018-07-24 17:18:00 +0530

    created basic routes setup

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,26 @@ const express = require('express');
 
 const router = express.Router();
 
-// to get list of ninjas
-router.get('/ninjas', function(req, res) {
 
-})
+// to get list of ninjas from the db
+router.get('/ninjas', function(req, res) {
+  res.send({type: "GET"});
+});
+
+
+// to add new ninjas in the db
+router.post('/ninjas', function(req, res) {
+  res.send({type: "POST"});
+});
+
+
+// to update a particular ninjas details in the db
+router.put('/ninjas/:id', function(req, res) {
+  res.send({type: "PUT"});
+});
+
+
+// to delete a particular ninjas from the db
+router.delete('/ninjas/:id', function(req, res) {
+  res.send({type: "DELETE"});
+});
